ec3a46cfc4c97a88a29e4b24dcdb65a164d0651d
Amrut Prabhu <amrut08@gmail.com>
2020-03-26 15:51:59 +0800

    Implement ranked retrieval search

--- ranked-retrieval/search.py
+++ ranked-retrieval/search.py
@@ -7,65 +7,109 @@ import getopt
 import linecache
 import _pickle as pickle
 import math
-
-from dictionary import Dictionary
-from postings_lists import PostingsLists
+import heapq
 
 def usage():
     print("usage: " + sys.argv[0] + " -d dictionary-file -p postings-file -q file-of-queries -o output-file-of-results")
 
 
-def process_query(query):
+def log10(x):
     """
-    Query terms processed using stemming and case folding.
+    Returns log base 10 for positive input, and zero otherwise.
     """
-    output = []
+    if x > 0:
+        return math.log(x, 10)
+    
+    return 0
+
+
+def read_postings_list_from_disk(offset, postings_fo):
+    """
+    Returns the postings list of the `word`, read from the postings file.
+    
+    Input:
+        offset: Position to start reading from in the file
+        postings_fo: File containing the postings lists
+
+    Output:
+        Postings list of document IDs and normalised term frequency 
+        weights [ (doc_id, n_wt), ... ]
+    """
+    postings_fo.seek(offset, 0)
+    postings = pickle.load(postings_fo)
+
+    return postings
+
+
+def calculate_document_scores(query_tfs, dictionary, postings_fo):
+    """
+    Returns the postings list of the `word`, read from the postings file.
+    
+    Input:
+        query_tfs: Raw term frequency counts for the query { word_type: tf }
+        dictionary: { word_type : (idf, offset_bytes) } 
+        postings_fo: File containing the postings lists
+
+    Output:
+        Dict with scores assigned to documents relevant to the 
+        query { doc_id: score }
+    """
+    doc_scores = {}
+
+    for term in query_tfs:
+        if term not in dictionary:
+            continue
+
+        # No need to normalize query weights since it is shared between all 
+        # document scores, and we just want relative scores
+        (idf, offset) = dictionary[term]
+        wt_tq = (1 + log10(query_tfs[term])) * idf 
+
+        postings = read_postings_list_from_disk(offset, postings_fo)
+        for (doc_id, n_wt_td) in postings:
+            if doc_id not in doc_scores:
+                doc_scores[doc_id] = 0
+            doc_scores[doc_id] += n_wt_td * wt_tq
+
+    return doc_scores
+
+
+def perform_search_query(query, dictionary, postings_fo):
+    """
+    Returns list of top ranked documents (up to K=10), sorted by decreasing 
+    relevance, and increasing doc ID.
+    Query terms are processed using stemming and case folding.
+
+    Input:
+        query: String of query terms
+
+    Output:
+        List of doc IDs, with maximum size of 10
+    """
+    K = 10
+
+    query_tfs = {}
 
     stemmer = nltk.stem.porter.PorterStemmer()
     tokens = nltk.word_tokenize(query)
     for token in tokens:
         # Apply stemming and case folding as done during indexing
-        output.append(stemmer.stem(token).lower())
+        term = stemmer.stem(token).lower()
 
-    return output
+        if term not in query_tfs:
+            query_tfs[term] = 0
+        query_tfs[term] = query_tfs[term] + 1
 
 
-def read_postings_list_from_disk(word, dictionary, postings_fo):
-    """
-    Returns the postings list of the `word`, read from the postings file.
-    """
-    # TODO: time - use pickle to load serialized data instead. 
-    # Store skip pointers on disk
-    # Use f.tell() to store offsets in index.py
-    (num_docs, offset, size, skip_len) = dictionary[word]
+    doc_scores = calculate_document_scores(query_tfs, dictionary, postings_fo)
     
-    postings_fo.seek(offset, 0)
-    postings = postings_fo.read(size)
-    postings = [int(docID) for docID in postings[:-1].split(',')] # Remove trailing space and convert to list
+    def sort_key(doc_score):
+        # Descending order of score, and uses smaller doc ID to break ties.
+        return -doc_score[1], doc_score[0]
 
-    return postings
+    search_results = heapq.nsmallest(K, doc_scores.items(), key=sort_key)
 
-
-# Wrapper tuple class to store list of document IDs and the length of the skip pointers for this list
-QueryResult = namedtuple('QueryResult', ['doc_Ids', 'skip_len'])
-
-
-def get_query_result(operand, dictionary, postings_fo):
-    """
-    Returns `operand` if it is a `QueryResult`. 
-    Otherwise, read from the disk and return a QueryResult containing the postings list 
-    of document IDs and length of skip pointers.
-    """
-    if type(operand) is QueryResult:
-        return operand
-
-    if operand not in dictionary:
-        return QueryResult([], 0)
-
-    postings_list = read_postings_list_from_disk(operand, dictionary, postings_fo)
-
-    return QueryResult(postings_list, dictionary[operand][-1])
-
+    return map(lambda x: x[0], search_results)
 
 
 def run_search(dict_file, postings_file, queries_file, results_file):
@@ -75,32 +119,18 @@ def run_search(dict_file, postings_file, queries_file, results_file):
     """
     print('running search on the queries...')
 
-    # { word_type : (num_docs, offset_bytes, size_bytes, skip_len) } 
+    # { word_type : (idf, offset_bytes) } 
     f = open(dict_file, 'rb')
     dictionary = pickle.load(f)
     f.close()
 
-    postings_fo = open(postings_file, "rt")
+    postings_fo = open(postings_file, "rb")
     results_fo = open(results_file, "wt")
 
     line_num = 1
     line = linecache.getline(queries_file, line_num)
     while line != '':
-        # TODO: lnc.ltc ranking scheme 
-        # i.e. For QUERIES documents, log tf and idf with cosine normalization
-        # For DOCUMENTS, log tf, cosine normalization but no idf 
-        
-        # Cosine similarity with weights tf-idf = (1 + math.log(tf, 10)) * math.log(N/df, 10) # TODO: Take care of math.log(0, 10)
-
-
-        query = shunting_yard(line) # process_query(line)
-        search_results = perform_search_query(query, dictionary, postings_fo)
-        
-        # TODO: output a list of UP TO 10 most relevant document IDs
-        # sorted by decreasing relevance and then doc ID (if same relevance)
-
-        # Use Max Heap- You could set the max-heap to contain only 100 documents (fewer, if you'd like) 
-        # and at the end of processing, just report the max 10
+        search_results = perform_search_query(line, dictionary, postings_fo)
 
         # Write results to output file
         search_results_str = ' '.join([str(docID) for docID in search_results]) + '\n'
